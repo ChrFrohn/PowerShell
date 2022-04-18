fd3b03033497367d31ec2c6c49e6e9ad3f7c0d5b
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2022-04-18 19:47:29 +0200

    Create CreateCertForAppReg.ps1

---
+++ CreateCertForAppReg.ps1
@@ -0,0 +1,10 @@
+$CN = "MyAppReg" #Name of your cert.
+$cert=New-SelfSignedCertificate -Subject "CN=$CN" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature
+
+#Thumbprint value
+$Thumbprint = $Cert.Thumbprint
+
+#Export cert. to users download folder
+Get-ChildItem Cert:\CurrentUser\my\$Thumbprint | Export-Certificate -FilePath $env:USERPROFILE\Downloads\AppRegCert.cer
+
+Write-Output "$Thumbprint <- Copy/paste this (save it)"
