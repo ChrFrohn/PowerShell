6059ca36d619a469e031fc6fefd4ca809ec1659b
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-04-30 12:30:42 +0200

    Update ConnectToMSGraphWithMIandKeyVault2.ps1

--- ConnectToMSGraphWithMIandKeyVault2.ps1
+++ ConnectToMSGraphWithMIandKeyVault2.ps1
@@ -1,21 +1,31 @@
+# Connect to Azure using af Managed Identity (VM)
 Connect-AzAccount -Identity 
 
-$KeyVaultName = "NameOfKeyVault"
-$KeyVaultSecretName = "SecretName"
-$KeyVaultTenantIDName = "TenantIDName"
-$KeyVaultClientIDName = "ClientIDName"
+$KeyVaultName = "" # The name of your Azure Key Vault
+$KeyVaultSecretName = "" # The name of the Secret entry in the Azure Key Vault
+$KeyVaultTenantIDName = "" # The name of the TenantID entry in the Azure Key Vault
+$KeyVaultClientIDName = "" # The name of the ClientID entry in the Azure Key Vault
 
+# Fetch the values from the Azure Key vault
 $KeyVaultSecret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -AsPlainText
 $KeyVaultTenantID = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultTenantIDName -AsPlainText
 $KeyVaultClientID = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultClientIDName -AsPlainText
 
+# Connect to the Microsoft Graph with the Service Principal usinge the values from the Azure Key vault
+$body =  @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $KeyVaultClientID
+    Client_Secret = $KeyVaultSecret
+}
+ 
+$connection = Invoke-RestMethod `
+    -Uri https://login.microsoftonline.com/$KeyVaultTenantID/oauth2/v2.0/token `
+    -Method POST `
+    -Body $body
+ 
+$GraphToken = ConvertTo-SecureString $connection.access_token -AsPlainText -Force
+Connect-MgGraph -AccessToken $GraphToken -Verbose
 
-$Body = @{   
-   Grant_Type    = "client_credentials"   
-   Scope         = "https://graph.microsoft.com/.default"   
-   Client_Id     = $KeyVaultClientID
-   Client_Secret = $KeyVaultSecret   
-}  
-
-$graphToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$KeyVaultTenantID/oauth2/v2.0/token" -Method POST -Body $Body | Select-Object -ExpandProperty Access_Token 
-Connect-MgGraph -AccessToken $Body 
+# Get users from the Graph
+Get-MgUser
