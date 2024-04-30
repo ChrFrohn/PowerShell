1468370967482db55b3a8857fac9beaf756be827
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-04-30 10:00:33 +0200

    Update ConnectToMSGraphWithMIandKeyVault.ps1

--- ConnectToMSGraphWithMIandKeyVault.ps1
+++ ConnectToMSGraphWithMIandKeyVault.ps1
@@ -1,16 +1,17 @@
-# This code is realted to following blog post:
-# 
+# Connect to Azure using af Managed Identity (VM)
 Connect-AzAccount -Identity 
+ 
+$KeyVaultName = "" # The name of your Azure Key Vault
+$KeyVaultSecretName = "" # The name of the Secret entry in the Azure Key Vault
+$KeyVaultTenantIDName = "" # The name of the TenantID entry in the Azure Key Vault
+$KeyVaultClientIDName = "" # The name of the ClientID entry in the Azure Key Vault
 
-$KeyVaultName = "NameOfKeyVault"
-$KeyVaultSecretName = "SecretName"
-$KeyVaultTenantIDName = "TenantIDName"
-$KeyVaultClientIDName = "ClientIDName"
-
+# Fetch the values from the Azure Key vault
 $KeyVaultSecret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -AsPlainText
 $KeyVaultTenantID = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultTenantIDName -AsPlainText
 $KeyVaultClientID = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultClientIDName -AsPlainText
- 
+
+# Connect to the Microsoft Graph with the Service Principal usinge the values from the Azure Key vault
 $body =  @{
     Grant_Type    = "client_credentials"
     Scope         = "https://graph.microsoft.com/.default"
@@ -23,12 +24,11 @@ $connection = Invoke-RestMethod `
     -Method POST `
     -Body $body
 
-
 $headers = @{
                 "Authorization" = "Bearer " + $connection.access_token
             }
-
 $url = "https://graph.microsoft.com/v1.0/users/"
-
+ 
 #Output all users
-Invoke-RestMethod -Uri $url -Headers $headers -Method Get -ErrorAction Stop  
+$AllUsers = Invoke-RestMethod -Uri $url -Headers $headers -Method Get -ErrorAction Stop  
+$AllUsers.value
