43be08372ad3d72a4f9f7556903fe7a43951619f
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-05-02 10:37:02 +0200

    Create GetEmployeeHireOrLeaveDateTime2.ps1

---
+++ GetEmployeeHireOrLeaveDateTime2.ps1
@@ -0,0 +1,19 @@
+# UPN or Object ID of the user 
+$ObjectId = "itnb@kromannreumert.com"
+
+# Import Module
+Import-Module Microsoft.Graph.Users
+
+# Microsoft Graph Permission scope
+$Scopes =@("User.Read.All", "User-LifeCycleInfo.Read.All")
+
+# Connect to the Microsoft Graph
+Connect-MgGraph -Scopes $Scopes
+
+# Get the users 
+$MgUser = Get-MgUser -UserId $ObjectId -Property employeeHireDate,employeeLeaveDateTime,EmployeeOrgData
+
+$MgUser.EmployeeHireDate
+$MgUser.EmployeeLeaveDateTime
+$MgUser.EmployeeOrgData.CostCenter
+$MgUser.EmployeeOrgData.Division
